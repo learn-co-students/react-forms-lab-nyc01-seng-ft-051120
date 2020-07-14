6cd3cd4a482210c682c366d81fc5cf03d295f900
grantkoji <grantkoji@gmail.com>
2020-07-14 15:55:34 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,57 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  // handleLogin={this.login}
+  // this.props.handleLogin 
+  // login = ({ username, password }) => {
+  //   console.log(`Logging in ${username} with password ${password}`);
+  // };
+  // this.props.handleLogin()
+
+  onChangeUsername = event => {
+    this.setState({
+      username: event.target.value 
+    })
+  }
+
+  onChangePassword = event => {
+    this.setState({
+      password: event.target.value 
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    if (this.state.username != '' && this.state.password != '') {
+      this.setState({
+        username: this.state.username,
+        password: this.state.password
+      })
+      this.props.handleLogin(this.state.username, this.state.password)
+    } else {
+      alert('You must enter a username and password')
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" onChange={event => this.onChangeUsername(event)}type="text" value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" onChange={event => this.onChangePassword(event)} type="password" value={this.state.password}/>
           </label>
         </div>
         <div>
