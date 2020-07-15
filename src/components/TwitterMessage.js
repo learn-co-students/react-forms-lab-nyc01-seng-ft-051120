2d4018afc11269c939f4fb7423ed3c4738b6ea2a
CMSTie <cmstie@gmail.com>
2020-07-15 16:32:06 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,19 +1,48 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      message: '',
+    };
   }
 
-  render() {
+  handleMessage = e => {
+    this.setState({
+      message: e.target.value
+    })
+  }
+
+  // showMessage = () => {
+  //   if (this.state.message.length <= this.props.maxChars){
+  //     return(
+        
+  //         // this.state.message
+  //       `Character counter: ${this.props.maxChars - this.state.message.length}`
+  //     )
+  //   }
+  //   else {
+  //     return(
+        
+  //         "Your message is too long."
+        
+  //         )
+  //       }
+  //     }
+      
+      render() {
+        // console.log(this.state.message.length);
+        // console.log(this.props.maxChars);
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={e => this.handleMessage(e)}/>
+        {/* {this.showMessage()} */}
+        <p>Character Counter: {this.props.maxChars - this.state.message.length}</p>
       </div>
-    );
+   );
   }
 }
 
